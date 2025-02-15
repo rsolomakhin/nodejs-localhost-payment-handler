1a568382147d0ee3186f9c4dcf2917490725a18e
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2025-02-15 00:40:20 +0000

    Fix the typo

--- payment-handler/server.js
+++ payment-handler/server.js
@@ -10,5 +10,5 @@ const server = http.createServer(function (req, res) {
 });
 
 server.listen(port, hostname, function () {
-  console.log('Payment handler server running at http://${hostname}:${port}/`);
+  console.log(`Payment handler server running at http://${hostname}:${port}/`);
 });
